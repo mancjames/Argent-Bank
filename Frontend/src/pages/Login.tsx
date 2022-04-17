4d6e23275d6da479262802ace0c0d6d65428d533
mancjames <multicolouredsmurf@gmail.com>
2022-04-17 22:20:44 +0100

    updated structure so backend and frontend are seperate

---
+++ Frontend/src/pages/Login.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Login({}: Props) {
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label>Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label>Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label>Remember me</label>
+          </div>
+          {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
+          <a href="./profile" className="sign-in-button">Sign In</a>
+          {/* <!-- SHOULD BE THE BUTTON BELOW -->
+          <!-- <button className="sign-in-button">Sign In</button> -->
+          <!--  --> */}
+        </form>
+      </section>
+    </main>
+  )
+}
